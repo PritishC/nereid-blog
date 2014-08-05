fb775148a7aded888e304e122186f412664a5f5b
VaticanCameos <chakrabortypritish@gmail.com>
2014-08-05 12:58:27 +0530

    Put configuration details in config.py. Imported config.py stuff in app/__init__.py.

---
+++ config.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  5 12:55:34 2014
+
+@author: pritishc
+"""
+
+import os
+from flask.ext.login import LoginManager
+from flask.ext.openid import OpenID 
+from app_run import app
+
+basedir = os.path.abspath(os.curdir)
+
+lm = LoginManager()
+lm.init_app(app)
+lm.login_view = 'login'
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
+
+CSRF_ENABLED = True
+
+OPENID_PROVIDERS = [
+    { 'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id' },
+    { 'name': 'Yahoo', 'url': 'https://me.yahoo.com' },
+    { 'name': 'AOL', 'url': 'http://openid.aol.com/<username>' },
+    { 'name': 'Flickr', 'url': 'http://www.flickr.com/<username>' },
+    { 'name': 'MyOpenID', 'url': 'https://www.myopenid.com' }]
