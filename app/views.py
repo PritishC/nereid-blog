c39a253219dde89a3eb917e1d923676c62e3a0c9
VaticanCameos <chakrabortypritish@gmail.com>
2014-08-05 13:24:45 +0530

    Added a login handler function. This has the openid loginhandler decorator to designate it as the login handling function. Need to add those auxiliary functions like is_authenticated to User class.

--- app/views.py
+++ app/views.py
@@ -33,3 +33,17 @@ def index():
                            user=user,
                            posts=posts)
 
+@app.route('/login', methods=['GET', 'POST'])
+@oid.loginhandler
+def login():
+    if g.user is not None and g.user.is_authenticated():
+        return redirect(url_for('index'))
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        session['remember_me'] = login_form.remember_me.data
+        return oid.try_login(login_form.openid.data, ask_for=['nickname', 'email'])
+    return render_template('login.html',
+                           title='Sign In',
+                           form=login_form,
+                           providers=app.config['OPENID_PROVIDERS'])
+    
